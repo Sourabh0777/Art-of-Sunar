1b431b27400b0fd978772d64edc37ea33ed061c6
Sourabh <sourabh.verma9770@gmail.com>
2024-05-21 12:21:24 +0530

    Updates

--- src/actions/delete-files.ts
+++ src/actions/delete-files.ts
@@ -1,14 +1,14 @@
-'use server'
+// 'use server'
 
-import { UTApi } from 'uploadthing/server'
+// import { UTApi } from 'uploadthing/server'
 
-const deleteFiles = async (key: string) => {
-  try {
-    const utapi = new UTApi()
-    await utapi.deleteFiles(key)
-  } catch (error) {
-    console.log(error)
-  }
-}
+// const deleteFiles = async (key: string) => {
+//   try {
+//     const utapi = new UTApi()
+//     await utapi.deleteFiles(key)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
-export default deleteFiles
+// export default deleteFiles
